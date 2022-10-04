92f4220ee234a66c35b77f5dfc30bebc1a459cfb
hrj2233 <hrj2233@gmail.com>
2022-10-04 18:51:53 +0900

    Setup

---
+++ server/index.ts
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+
+// middleware
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(morgan('dev'));
+app.use(cookieParser());
+
+// routes
+app.get('/', (req, res) => {
+	res.json({ message: 'Home' });
+});
+
+// Database
+import './config/database';
+
+// server listening
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+	console.log('Server is running on port', PORT);
+});
